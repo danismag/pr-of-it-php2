133eb3b0dd98aa6c7b64508f210fb9704f0e664a
danis <danis29@bk.ru>
2017-01-18 20:44:31 +0300

    реализация интерфейса Iterator

--- App/View.php
+++ App/View.php
@@ -15,17 +15,9 @@ use App\Traits\TMagicProperties;
  * @property array lastNews
  */
 class View
-    implements \Countable
+    implements \Countable, \Iterator
 {
     use TMagicProperties;
-
-    /**
-     * Реализация интерфейса Countable
-     */
-    public function count()
-    {
-        return count($this->data);
-    }
 
     /**
      * Подставляет в шаблон переданные данные и возвращает html
@@ -49,5 +41,70 @@ class View
     public function display($template)
     {
         echo $this->render($template);
+    }
+
+    /**
+     * Реализация интерфейса Countable
+     */
+    public function count()
+    {
+        return count($this->data);
+    }
+
+    /**
+     * Реализация интерфейса Iterator
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return current($this->data);
+    }
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        next($this->data);
+    }
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return key($this->data);
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return false !== current($this->data);
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        reset($this->data);
     }
 }
