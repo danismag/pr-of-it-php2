d251a056bbf24d910d6ffae532d37e8b1b291a77
danis <danis29@bk.ru>
2017-01-20 11:06:34 +0300

    autoload.php + index.php

--- App/autoload.php
+++ App/autoload.php
@@ -4,5 +4,8 @@ function __autoload($className)
 {
     $filename = str_replace('\\', '/', $className) . '.php';
     $path =  __DIR__ . '/../' . $filename;
-    require $path;
+    if (file_exists($path)) {
+
+        require $path;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -2,8 +2,20 @@
 
 require __DIR__ . '/../App/autoload.php';
 
-use App\View, App\Models\Article;
+$parts = explode('/', $_SERVER['REQUEST_URI']);
+
+$controllerName = $parts[1] ?: 'Index';
+$actionName =     $parts[2] ?: 'Default';
+$controllerClass = '\\App\\Controllers\\' . $controllerName;
+
+if (!class_exists($controllerClass)) {
+    die('Контроллер ' . $controllerClass . ' не найден');
+}
+$controller = new $controllerClass;
+$controller->action($actionName);
+
+/*use App\View, App\Models\Article;
 
 $view = new View;
 $view->lastNews = Article::getLast(3);
-$view->display(__DIR__ . '/../App/Templates/mainPage.php');
+$view->display(__DIR__ . '/../App/Templates/mainPage.php');*/
