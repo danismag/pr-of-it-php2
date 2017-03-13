56c871ba02350484503a9428fd53b67349d84b4d
danismag <danismag@bk.ru>
2017-03-13 20:30:19 +0300

    + getTagsValue()

--- App/Models/TagsExtractor.php
+++ App/Models/TagsExtractor.php
@@ -7,6 +7,7 @@ namespace App\Models;
 class TagsExtractor
 {
     protected $text;
+//\[(?P<tag1>\w+):?(?P<desc1>.+)?\](?P<tagval>.+)\[\/(?P=tag1)\]
 
     public function __construct($text)
     {
@@ -15,8 +16,17 @@ class TagsExtractor
 
     public function getTagsValue():array
     {
-        preg_match('',
-            $this->text, $result);
+        $result = [];
+        $text = $this->text;
+        while (preg_match(
+                '~\[(?P<tag>\w+):?.*\](?P<tagval>.+)\[\/(?P=tag)\]~',
+                $text,
+                $matches)) {
+
+            $result[$matches['tag']] = $result[$matches['tagval']];
+            $matches = [];
+            $text = preg_replace('~.*\[.*\].*\[\/.*\]~', '', $text);
+        }
         return $result;
     }
 
